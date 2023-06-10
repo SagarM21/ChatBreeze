163d2084ba9c531d5b363c6c40745fc085503634
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-06-10 12:16:38 +0530

    add: btn in authScreen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -10,6 +10,7 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var _isLogin = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +46,25 @@ class _AuthScreenState extends State<AuthScreen> {
                         decoration:
                             const InputDecoration(labelText: 'Password'),
                         obscureText: true,
-                      )
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.primary),
+                          child: Text(_isLogin ? 'Login' : 'Signup')),
+                      TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: Text(_isLogin
+                              ? 'Create an Account'
+                              : 'I already have an account'))
                     ],
                   )),
                 )),
